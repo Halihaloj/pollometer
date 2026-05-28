635c9ebda58f916c7ecdd8906514051913f0d735
Anna Baltzer Rose <annabaltzerrose@gmail.com>
2026-05-28 15:45:20 +0200

    cleanup

--- webapp_sqlite.py
+++ webapp_sqlite.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Setup web page layout
 st.set_page_config(page_title="pollometeret", page_icon="🌱", layout="wide")
-st.title("🌱 Pollometeret - Københavns Pollendata")
+st.title("🌱 Pollometeret - Overblik over de Københavnske pollendata")
 
 # Connection to SQLite db
 conn = st.connection("sqlite", type="sql", url="sqlite:///pollen_data.db")
@@ -34,12 +34,12 @@ df = conn.query(sql_query, ttl="0")
 # Display metric boxes at top of page
 col1, col2 = st.columns(2)
 with col1:
-    st.metric(label="Observationer", value=len(df))
+    st.metric(label="Antal målinger for valgte lokation:", value=len(df))
 with col2:
     # Clean up Concentration to numeric just in case there are text strings left
     numeric_concentration = pd.to_numeric(df["concentration"].astype(str).str.extract(r'(\d+)', expand=False), errors='coerce')
-    st.metric(label="Højest målte koncentration", value=int(numeric_concentration.max()) if not numeric_concentration.isna().all() else 0)
+    st.metric(label="Højest målte koncentration:", value=int(numeric_concentration.max()) if not numeric_concentration.isna().all() else 0)
 
 # Print interactive data table onto the webpage
-st.subheader(f"Viser resultater for lokation: {selected_location}")
+st.subheader(f"Viser resultater for {selected_location}")
 st.dataframe(df, use_container_width=True)
