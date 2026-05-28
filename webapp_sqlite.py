31eaf309c5f09549943e801edf40207c4ab42796
halihaloj <Oskarbigum1@gmail.com>
2026-05-28 16:11:51 +0200

    pollentype

--- webapp_sqlite.py
+++ webapp_sqlite.py
@@ -11,35 +11,50 @@ conn = st.connection("sqlite", type="sql", url="sqlite:///pollen_data.db")
 # ocation sidebar
 st.sidebar.header("Filtrer pollendata")
 
+location_list = []
+
 # Fetch locations to build dropdown menu
 try:
-    location_query = conn.query('SELECT DISTINCT "location" FROM pollen;')
+    location_query = conn.query('SELECT DISTINCT "location" FROM pollen_measurements;')
     location_list = location_query["location"].dropna().tolist()
+
+    pollen_query = conn.query('SELECT DISTINCT "pollen_type" FROM pollen_measurements; ')
+    pollen_list = pollen_query["pollen_type"].dropna().tolist()
 except Exception:
     # Fallback in case DB has different name
-    st.error("Could not find table named 'pollen' in database.")
+    st.error("Could not find table named 'pollen_measurements' in database.")
     st.stop()
 
 selected_location = st.sidebar.selectbox("Vælg lokation:", ["Alle lokationer"] + location_list)
+selected_pollen = st.sidebar.selectbox("Vælg pollen type", ["Alle pollentyper"]+pollen_list)
 
 # SQL query based on selected location
-if selected_location == "Alle lokationer":
-    sql_query = 'SELECT * FROM pollen;'
-else:
-    sql_query = f'SELECT * FROM pollen WHERE "location" = \'{selected_location}\';'
+base_query = 'SELECT * FROM pollen_measurements WHERE 1=1'
+query_params = {}
 
+# If a specific location is selected, add it to the SQL query
+if selected_location != "Alle lokationer":
+    base_query += ' AND "location" = :location'
+    query_params["location"] = selected_location
+
+# If a specific pollen type is selected, add it to the SQL query
+if selected_pollen != "Alle pollentyper":
+    base_query += ' AND "pollen_type" = :pollen_type'
+    query_params["pollen_type"] = selected_pollen
+
+    
 # Fetch table from SQLite, cache turned off
-df = conn.query(sql_query, ttl="0")
+filtered_df = conn.query(base_query, params=query_params, ttl="0")
 
 # Display metric boxes at top of page
-col1, col2 = st.columns(2)
-with col1:
-    st.metric(label="Observationer", value=len(df))
-with col2:
+loc_col1, loc_col2 = st.columns(2)
+with loc_col1:
+    st.metric(label="Observationer", value=len(filtered_df))
+with loc_col2:
     # Clean up Concentration to numeric just in case there are text strings left
-    numeric_concentration = pd.to_numeric(df["concentration"].astype(str).str.extract(r'(\d+)', expand=False), errors='coerce')
+    numeric_concentration = pd.to_numeric(filtered_df["concentration"].astype(str).str.extract(r'(\d+)', expand=False), errors='coerce')
     st.metric(label="Højest målte koncentration", value=int(numeric_concentration.max()) if not numeric_concentration.isna().all() else 0)
 
 # Print interactive data table onto the webpage
 st.subheader(f"Viser resultater for lokation: {selected_location}")
-st.dataframe(df, use_container_width=True)
+st.dataframe(filtered_df, use_container_width=True)
